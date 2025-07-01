ae5f0bf4e216425c4d13e6a092a0e417f4c73611
Felipe Leite <felipe_o_leite@hotmail.com>
2025-06-30 21:40:45 -0300

    modelagem banco

---
+++ db_schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE cliente (
+                         id SERIAL PRIMARY KEY,
+                         nome_completo VARCHAR(100) NOT NULL,
+                         cpf VARCHAR(11) NOT NULL UNIQUE,
+                         data_nascimento DATE NOT NULL,
+                         email VARCHAR(100)
+);
+
+CREATE TABLE divida (
+                        id SERIAL PRIMARY KEY,
+                        cliente_id INTEGER NOT NULL REFERENCES cliente(id) ON DELETE CASCADE,
+                        valor NUMERIC(10,2) NOT NULL CHECK (valor > 0),
+                        situacao BOOLEAN NOT NULL DEFAULT FALSE,
+                        data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                        data_pagamento TIMESTAMP,
+                        descricao VARCHAR(255)
+);
+
