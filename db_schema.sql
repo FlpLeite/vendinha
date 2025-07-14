22c184008128bee7b47db7b0a6dd4b629b37f0f0
Felipe Leite <felipe_o_leite@hotmail.com>
2025-07-14 09:28:06 -0300

    ajustado arquivo db

--- db_schema.sql
+++ db_schema.sql
@@ -1,19 +1,9 @@
-CREATE TABLE cliente (
-                         id SERIAL PRIMARY KEY,
-                         nome_completo VARCHAR(100) NOT NULL,
-                         cpf VARCHAR(11) NOT NULL UNIQUE,
-                         data_nascimento DATE NOT NULL,
-                         email VARCHAR(100)
-);
-
-CREATE TABLE divida (
-                        id SERIAL PRIMARY KEY,
-                        cliente_id INTEGER NOT NULL REFERENCES cliente(id) ON DELETE CASCADE,
-                        valor NUMERIC(10,2) NOT NULL CHECK (valor > 0),
-                        situacao BOOLEAN NOT NULL DEFAULT FALSE,
-                        data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                        data_pagamento TIMESTAMP,
-                        descricao VARCHAR(255)
+CREATE TABLE clientes (
+                          id SERIAL PRIMARY KEY,
+                          nome_completo VARCHAR(100) NOT NULL,
+                          cpf VARCHAR(11) NOT NULL UNIQUE,
+                          data_nascimento DATE NOT NULL,
+                          email VARCHAR(100)
 );
 
 CREATE TABLE usuarios (
@@ -24,12 +14,14 @@ CREATE TABLE usuarios (
                           senha_hash VARCHAR(100) NOT NULL
 );
 
-
-ALTER TABLE dividas ADD COLUMN criado_por_id INTEGER REFERENCES usuarios(id);
-ALTER TABLE dividas ADD COLUMN pago_por_id   INTEGER REFERENCES usuarios(id);
-
-
-ALTER TABLE divida RENAME TO dividas;
-
-ALTER SEQUENCE cliente_id_seq RENAME TO clientes_id_seq;
-ALTER SEQUENCE divida_id_seq   RENAME TO dividas_id_seq;
+CREATE TABLE dividas (
+                         id SERIAL PRIMARY KEY,
+                         cliente_id INTEGER NOT NULL REFERENCES clientes(id) ON DELETE CASCADE,
+                         valor NUMERIC(10,2) NOT NULL CHECK (valor > 0),
+                         situacao BOOLEAN NOT NULL DEFAULT FALSE,
+                         data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         data_pagamento TIMESTAMP,
+                         descricao VARCHAR(255),
+                         criado_por_id INTEGER REFERENCES usuarios(id),
+                         pago_por_id   INTEGER REFERENCES usuarios(id)
+);
